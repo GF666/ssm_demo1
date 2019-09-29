e71af384e8ac1103dcba134bac97a80d799d1d1b
GF666 <646294548@qq.com>
2019-09-29 21:02:37 +0800

    进行测试

---
+++ studentManager/src/main/java/com/cskaoyan/SsmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmDemoApplication.class, args);
+    }
+
+}
